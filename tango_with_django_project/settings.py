517d4a75474656bf7458fbd45928635fc16ca419
yaasir ali <2330228a@student.gla.ac.uk>
2018-01-23 13:24:38 +0000

    Chapter 3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',	'rango',
+    'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE_CLASSES = [
